142169942f6005ecd60fa7f83dbeb2bae2cb66d0
Nikkiz2 <nikkiz2@nycstudents.net>
2024-12-08 00:32:34 -0500

    Revert "Merge"

--- src/Time.java
+++ src/Time.java
@@ -8,7 +8,6 @@ public class Time {
     private Timer myTimer = new Timer();
     private TimerTask task;
 
-    //creates the seconds timer
     public Time(Score score) {
         this.score = score;
         task = new TimerTask() {
