305c2f62eb2654f1f209b620db40516a6b104c4e
Myrna <myrnaz@nycstudents.net>
2024-12-08 20:39:30 -0500

    did some variabel chanhing

--- src/Trash.java
+++ src/Trash.java
@@ -1,11 +1,5 @@
 public class Trash {
-    private String trash1;
-    private int trash1Row;
-    private int trash2Row;
-    private int trash3Row;
-    private int trash1Col;
-    private int trash2Col;
-    private int trash3Col;
+    final private String trash1;
 
     public Trash() {
         trash1 = "\uD83D\uDDD1\uFE0F";
@@ -20,19 +14,19 @@ public class Trash {
     }
 
     public int getTrash2Row() {
-        int row = (int) (Math.random()*5);
-        while (row == getTrash1Row()) {
-            row = (int) (Math.random()*5);
+        int trash2Row = (int) (Math.random()*5);
+        while (trash2Row == getTrash1Row()) {
+            trash2Row = (int) (Math.random()*5);
         }
-        return row;
+        return trash2Row;
     }
 
     public int getTrash3Row() {
-        int row = (int) (Math.random()*5);
-        while ((row == getTrash1Row()) || (row == getTrash2Row())) {
-            row = (int) (Math.random()*5);
+        int trash3Row = (int) (Math.random()*5);
+        while ((trash3Row == getTrash1Row()) || (trash3Row == getTrash2Row())) {
+            trash3Row = (int) (Math.random()*5);
         }
-        return row;
+        return trash3Row;
     }
 
     public int getTrash1Col() {
@@ -40,18 +34,18 @@ public class Trash {
     }
 
     public int getTrash2Col() {
-        int col = (int) (Math.random()*3) + 1;
-        while (col == getTrash1Col()) {
-            col = (int) (Math.random()*3) + 1;
+        int trash2Col = (int) (Math.random()*3) + 1;
+        while (trash2Col == getTrash1Col()) {
+            trash2Col = (int) (Math.random()*3) + 1;
         }
-        return col;
+        return trash2Col;
     }
 
     public int getTrash3Col() {
-        int col = (int) (Math.random()*3) + 1;
-        while ((col == getTrash1Col()) || (col == getTrash2Col())) {
-            col = (int) (Math.random()*3) + 1;
+        int trash3Col = (int) (Math.random()*3) + 1;
+        while ((trash3Col == getTrash1Col()) || (trash3Col == getTrash2Col())) {
+            trash3Col = (int) (Math.random()*3) + 1;
         }
-        return col;
+        return trash3Col;
     }
 }
